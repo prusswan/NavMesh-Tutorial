bf01e7f5bf451c289e3991fe2bef0920ebf40193
prusswan <prusswan@gmail.com>
2018-11-21 13:14:38 +0800

    example 1 - agent responds to click destination

---
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Camera cam;
+	public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				// MOVE OUR AGENT
+				agent.SetDestination(hit.point);
+			}
+		}
+		
+	}
+}
